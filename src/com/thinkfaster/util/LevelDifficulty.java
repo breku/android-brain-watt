bb4fc0d5f3b929c8e1bff038b1aca8b4a751fb90
Breku <jakub.brek@gmail.com>
2014-01-10 15:06:22 +0100

    LifeBar Speed

--- src/com/thinkfaster/util/LevelDifficulty.java
+++ src/com/thinkfaster/util/LevelDifficulty.java
@@ -6,8 +6,8 @@ package com.thinkfaster.util;
  */
 public enum LevelDifficulty {
     EASY(20, false, -14.0f),
-    MEDIUM(100, false, -16.0f),
-    HARD(100, true, -18.0f);
+    MEDIUM(100, false, -15.0f),
+    HARD(100, true, -16.0f);
 
     private Integer randomSeedSize;
     private Boolean minusAllowed;
